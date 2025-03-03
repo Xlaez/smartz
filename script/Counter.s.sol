6eb645507b806038ed3cc2eb70e681f44b572cf9
Utee Ekong <utibeabasiekong2@gmail.com>
2025-03-03 03:48:35 +0100

    update: add deploy script

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -2,17 +2,35 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
-import {Counter} from "../src/Counter.sol";
+
+import "../src/Marketplace.sol";
+import "../src/MarketPlaceNftItem.sol";
+import "../src/Profile.sol";
+import "../src/interfaces/IERC6551Registry.sol";
+import {IAccountProxy} from "../src/interfaces/IImplementation.sol";
 
 contract CounterScript is Script {
-    Counter public counter;
-
     function setUp() public {}
 
     function run() public {
-        vm.startBroadcast();
+        uint256 privateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(privateKey);
 
-        counter = new Counter();
+        // Deploy marketplace contract
+        MarketPlace marketplace = new MarketPlace(payable(msg.sender));
+        console.log("Marketplace deployed at:", address(marketplace));
+
+        // Deploy NFT contract
+        MarketPlaceItemNft nft = new MarketPlaceItemNft(
+            msg.sender,
+            "SmartzNFT",
+            "SNFT"
+        );
+        console.log("NFT contract deployed at:", address(nft));
+
+        // Deploy Profile contract
+        Profile profile = new Profile(address(marketplace));
+        console.log("Profile contract deployed at:", address(profile));
 
         vm.stopBroadcast();
     }
